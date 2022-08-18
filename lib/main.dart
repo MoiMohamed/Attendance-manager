89dd1a5a4022e8f2ab3b06bf6762b2f355fe7279
MoiMohamed <mohamed.ghoname314@gmail.com>
2022-08-18 22:23:35 +0200

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:attendance/providers/Auth.dart';
+import 'package:attendance/providers/Sessions.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 void main() => runApp(MyApp());
 
-
-class MyApp extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context)
-  {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (ctx)=>Auth())
+        ChangeNotifierProvider(create: (ctx) => Auth()),
+        ChangeNotifierProxyProvider<Auth, Sessions>(
+            update: (ctx, auth, prev) => Sessions(auth.tokenGet, auth.userIdGet,
+                prev == null ? [] : prev.sessions))
       ],
-      child: MaterialApp(
-          
-      ),
-
+      child: MaterialApp(),
     );
   }
 }
